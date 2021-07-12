4dca2b3f422e5afb100bcb1146682adeb8a33584
Hassan Mallah <hassanmallah02@gmail.com>
2021-07-12 22:51:44 +0500

    delete unnecessary codes

--- project.js
+++ project.js
@@ -3,16 +3,12 @@
 const closedFace = document.querySelector('.closed');
 const openFace = document.querySelector('.open');
 const bodys = document.querySelector('body');
-// const color = ['firebrick', 'aqua', 'coral', 'blueviolet'];
 
 // FUnctions
 function closedFaceFn() {
   if (closedFace.classList.contains('closed')) {
     closedFace.classList.add('active');
     openFace.classList.remove('active');
-
-    // const colorIndex = parseInt(Math.random() * color.length);
-    // body.style.backgroundColor = color[colorIndex];
   }
 }
 
@@ -20,9 +16,6 @@ function openFaceFn() {
   if (openFace.classList.contains('open')) {
     openFace.classList.add('active');
     closedFace.classList.remove('active');
-
-    // const colorIndex = parseInt(Math.random() * color.length);
-    // body.style.backgroundColor = color[colorIndex];
   }
 }
 
